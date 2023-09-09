1d30b61d5ca626d618d5c0b2adb53738af4c85d8
Rokas <rookas.rudzenskas@gmail.com>
2023-09-09 09:50:45 +0300

    🐛 fix(workspace.xml): update duration of a work item ✨ feat(player.tsx): add functionality to like/unlike a track and update UI accordingly

--- src/components/ui/player/player.tsx
+++ src/components/ui/player/player.tsx
@@ -4,50 +4,117 @@ import {usePlayerContext} from "../../../providers/player-provider";
 import {useEffect, useState} from "react";
 import {Sound} from "expo-av/build/Audio/Sound";
 import {Audio, AVPlaybackStatus} from "expo-av";
+import {gql, useMutation, useQuery} from "@apollo/client";
+
+const insertFavoriteMutation = gql`
+    mutation MyMutation($userId: String!, $trackId: String!) {
+        insertFavorites(userid: $userId, trackid: $trackId) {
+            id
+            trackid
+            userid
+        }
+    }
+`;
+
+const removeFavoriteMutation = gql`
+    mutation MyMutation($trackId: String!, $userId: String!) {
+        deleteFavorites(trackid: $trackId, userid: $userId) {
+            id
+        }
+    }
+`;
+
+const isFavoriteQuery = gql`
+    query MyQuery($trackId: String!, $userId: String!) {
+        favoritesByTrackidAndUserid(trackid: $trackId, userid: $userId) {
+            id
+            trackid
+            userid
+        }
+    }
+`;
 
 const Player = () => {
-  const { track } = usePlayerContext();
   const [sound, setSound] = useState<Sound>();
   const [isPlaying, setIsPlaying] = useState(false);
+  const { track } = usePlayerContext();
 
-  if(!track) return null;
-  const image = track.album.images?.[0];
-  //
-  // useEffect(() => {
-  //   playTrack();
-  // }, [track]);
+  const [insertFavorite] = useMutation(insertFavoriteMutation);
+  const [removeFavorite] = useMutation(removeFavoriteMutation);
+
+  const { data, refetch } = useQuery(isFavoriteQuery, {
+    variables: { userId: 'vadim', trackId: track?.id || '' },
+  });
+  const isLiked = data?.favoritesByTrackidAndUserid?.length > 0;
+
+  useEffect(() => {
+    playTrack();
+  }, [track]);
+
+  useEffect(() => {
+    return sound
+      ? () => {
+        console.log('Unloading Sound');
+        sound.unloadAsync();
+      }
+      : undefined;
+  }, [sound]);
 
   const playTrack = async () => {
-    if(sound) {
+    if (sound) {
       await sound.unloadAsync();
     }
 
-    if(!track.preview_url) return;
-
-    const {sound: newSound} = await Audio.Sound.createAsync({
+    if (!track?.preview_url) {
+      return;
+    }
+    const { sound: newSound } = await Audio.Sound.createAsync({
       uri: track.preview_url,
     });
 
     setSound(newSound);
     newSound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
-
     await newSound.playAsync();
-  }
+  };
 
   const onPlaybackStatusUpdate = (status: AVPlaybackStatus) => {
-    if(!status.isLoaded) return;
+    if (!status.isLoaded) {
+      return;
+    }
+
     setIsPlaying(status.isPlaying);
-  }
+  };
 
   const onPlayPause = async () => {
-    if(!sound) return;
-
-    if(isPlaying) {
+    if (!sound) {
+      return;
+    }
+    if (isPlaying) {
       await sound.pauseAsync();
     } else {
       await sound.playAsync();
     }
+  };
+
+  const onLike = async () => {
+    if (!track) return;
+    if (isLiked) {
+      await removeFavorite({
+        variables: { userId: 'rokas', trackId: track.id },
+      });
+    } else {
+      await insertFavorite({
+        variables: { userId: 'rokas', trackId: track.id },
+      });
+    }
+    refetch();
+  };
+
+  if (!track) {
+    return null;
   }
+
+  const image = track.album.images?.[0];
 
   return (
     <View className="absolute w-full top-[-75px] p-2 h-[75px]">
@@ -59,12 +126,18 @@ const Player = () => {
           <Text className="text-gray-400 text-[12px]">{track.artists[0]?.name}</Text>
         </View>
 
-        <Ionicons
-          name={'heart-outline'}
-          size={20}
-          color={'white'}
-          style={{ marginHorizontal: 10 }}
-        />
+        <TouchableOpacity
+          onPress={() => onLike()}
+          activeOpacity={0.7}
+        >
+          <Ionicons
+            name={isLiked ? 'heart' : 'heart-outline'}
+            size={20}
+            color={'white'}
+            style={{ marginHorizontal: 10 }}
+          />
+        </TouchableOpacity>
+
         <TouchableOpacity
           onPress={() => onPlayPause()}
           activeOpacity={0.7}
