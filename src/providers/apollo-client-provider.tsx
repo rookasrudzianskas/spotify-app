c8df9fedeb4002462af97fc68502c8b2b0e27269
Rokas <rookas.rudzenskas@gmail.com>
2023-09-09 09:19:57 +0300

    🔧 chore(workspace.xml): remove unnecessary changes from the default change list 🔧 chore(apollo-client-provider.tsx): update StepZen API endpoint and API key for the Apollo client

--- src/providers/apollo-client-provider.tsx
+++ src/providers/apollo-client-provider.tsx
@@ -2,10 +2,10 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { PropsWithChildren } from 'react';
 
 const client = new ApolloClient({
-  uri: 'https://<ACCOUNT_NAME>.stepzen.net/api/<ENDPOINT_NAME>/__graphql',
+  uri: 'https://saltillo.stepzen.net/api/irreverent-goat/__graphql',
   headers: {
     Authorization:
-      'apikey <YOUR_API_KEY>',
+      'apikey saltillo::stepzen.net+1000::c5e3ef401778df7e594c5ffa46283fea6ed840d5a4b5f489e81d8ced13cbe9bf',
   },
   cache: new InMemoryCache(),
 });
