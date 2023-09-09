f7e36406186c6862a1cbf540060bbf15b9ad3ab7
Rokas <rookas.rudzenskas@gmail.com>
2023-09-09 09:25:34 +0300

    🔧 chore(workspace.xml): update file path in ChangeListManager to reflect correct file location 🔧 chore(workspace.xml): update duration of workItem in task to reflect accurate time spent 🔧 chore(index.tsx): remove unused import and update query to fetch recommendations based on genres ✨ feat(index.tsx): add loading and error handling logic to display loading indicator and error message respectively ✨ feat(index.tsx): update FlatList to render tracks fetched from query response

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,48 @@
-import {View, FlatList} from 'react-native';
+import {View, FlatList, ActivityIndicator, Text} from 'react-native';
 import {StatusBar} from "expo-status-bar";
-import { tracks } from '../../../assets/data/tracks';
 import TrackListItem from "../../components/ui/track-list-item";
+import {gql, useQuery} from "@apollo/client";
 
+const query = gql`
+    query MyQuery($genres: String!) {
+        recommendations(seed_genres: $genres) {
+            tracks {
+                id
+                name
+                preview_url
+                artists {
+                    id
+                    name
+                }
+                album {
+                    id
+                    name
+                    images {
+                        url
+                        width
+                        height
+                    }
+                }
+            }
+        }
+    }
+`;
 
 export default function TabOneScreen() {
+  const { data, loading, error } = useQuery(query, {
+    variables: { genres: 'drum-and-bass,house' },
+  });
+
+  if (loading) {
+    return <View className="flex flex-1 items-center justify-center"><ActivityIndicator /></View>;
+  }
+
+  if (error) {
+    return (<Text style={{ color: 'white' }}>Failed to fetch recommendations</Text>);
+  }
+
+  const tracks = data?.recommendations?.tracks || [];
+
   return (
     <View className="flex-1 m-5">
       <FlatList
