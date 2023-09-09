0054267d8203800f70d990c996404c95f784a0d0
Rokas <rookas.rudzenskas@gmail.com>
2023-09-09 10:00:46 +0300

    🔧 chore(workspace.xml): update workspace.xml to reflect changes in file paths 🚀 feat(favorites.tsx): add FlatList component and gql query for fetching favorites data 🔧 chore(index.graphql): add favoritesByUserid query and update deleteFavorites mutation signature

--- src/app/(tabs)/favorites.tsx
+++ src/app/(tabs)/favorites.tsx
@@ -1,13 +1,48 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, FlatList} from 'react-native';
+import {gql} from "@apollo/client";
+import TrackListItem from "../../components/ui/track-list-item";
+import {tracks} from "../../../assets/data/tracks";
+
+const query = gql`
+    query getFavorites($userId: String!) {
+        favoritesByUserid(userid: $userId) {
+            id
+            trackid
+            userid
+            track {
+                id
+                name
+                preview_url
+                artists {
+                    id
+                    name
+                }
+                album {
+                    id
+                    name
+                    images {
+                        url
+                        width
+                        height
+                    }
+                }
+            }
+        }
+    }
+`;
 
 const Favorites = () => {
   return (
-    <View>
-      <Text>
-        byrookas 🚀
-      </Text>
+    <View className="mx-5 mt-5">
+      <FlatList
+        data={tracks}
+        renderItem={({ item }) => <TrackListItem track={item} />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 75 }}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 };
